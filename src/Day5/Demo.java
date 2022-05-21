b1d87d50340a9a2dbf593e98199bb12a5b7566ea
GautamViki <vikacse@gmail.com>
2022-05-22 02:06:00 +0530

    Ini

--- src/Day5/Demo.java
+++ src/Day5/Demo.java
@@ -3,5 +3,6 @@ package Day5;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("vikas");
+        System.out.println("jdfkhdskghsdlghsdlghsl");
     }
 }
